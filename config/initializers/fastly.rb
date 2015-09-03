dece699fa1743e11c4a6340f30234f970a2f0c07
Ezekiel Templin <zeke@templ.in>
2015-09-02 17:25:04 -0700

    Leverage purging_enabled config variable

--- config/initializers/fastly.rb
+++ config/initializers/fastly.rb
@@ -2,4 +2,5 @@ FastlyRails.configure do |c|
   c.api_key = ENV['FASTLY_API_KEY']
   # c.max_age = 86400 # 1 day
   c.service_id = ENV['FASTLY_SERVICE_ID']
+  c.purging_enabled = false unless ENV['ENABLE_PURGE']
 end
