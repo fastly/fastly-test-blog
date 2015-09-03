6283feb0c2c742f5c1486f25883140caf822f068
Ezekiel Templin <zeke@templ.in>
2015-09-02 17:32:10 -0700

    Add purging callbacks to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,10 @@
 class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
 
+  after_create  :purge_all
+  after_save    :purge
+  after_destroy :purge, :purge_all
+
   # Creates a new Comment, associated with an article
   def create_random_comment
     self.comments.create(name: Faker::Name.name, body: Faker::Hacker.say_something_smart)
