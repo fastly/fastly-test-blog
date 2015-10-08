50a52620d0cfd7e659fb0d58758ae3c052ea4dfb
Kami Richey <mrs.kami.richey@gmail.com>
2015-10-08 16:49:01 -0700

    test pass

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -5,5 +5,17 @@ class ArticlesControllerTest < ActionController::TestCase
     assert_difference('Article.count') do
       get :create_article
     end
+
+    assert_redirected_to(articles_url)
+
+  end
+
+  test 'article show' do
+    article = articles(:one)
+
+    get :show, id: article
+
+    assert_equal article.record_key, response.headers["Surrogate-Key"]
+    assert_response(:success)
   end
 end
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionController::TestCase
-  test 'should get about' do
+  test 'gets about page' do
     get :about
     assert_response :success
   end
