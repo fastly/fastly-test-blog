51fe1c886f63f688edfa0ca5d1f265e522422d27
Estella Madison <emadison@fastly.com>
2021-03-04 21:01:04 -0700

    Update Rails

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  skip_before_filter :verify_authenticity_token
+  protect_from_forgery
 end
